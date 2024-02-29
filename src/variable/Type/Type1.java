58c6ef8bc68df36b0da14917a9c1bb27a56f81d7
Hwangsj-qk <sujeong7191@naver.com>
2024-02-29 16:22:29 +0900

    타입 공부

---
+++ src/variable/Type/Type1.java
@@ -0,0 +1,11 @@
+package variable.Type;
+
+public class Type1 {
+    public static void main(String[] args) {
+        int a = 100;    // 정수 타입
+        double b = 20.5; // 실수 타입 (정수를 제외한 수)
+        boolean c = true;   // 불리언: true or false
+        char d = 'A';       // 캐릭터: '문자 하나'를 작은 따옴표 안에 사용
+        String e = "안녕하세요";     // 문자열: 끝따옴표 안에 사용
+    }
+}
