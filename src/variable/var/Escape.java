b2cd070cd6cf35042f75910e47be1c6e1aed092e
Hwangsj-qk <sujeong7191@naver.com>
2024-02-29 16:22:37 +0900

    이스케이프 문자 공부

---
+++ src/variable/var/Escape.java
@@ -0,0 +1,11 @@
+package variable.var;
+
+public class Escape {
+    public static void main(String[] args) {
+        // 이스케이프 문자: 문자열 내부에 역슬래시가 붙은 문자, 문자열 내부에 특정 문자 포함 가능
+
+        // 문자열 안에서 큰따옴표 사용법
+        String str = "나는 \"자바\"를 좋아합니다. ";
+        // 탭문자 사용: \
+    }
+}
