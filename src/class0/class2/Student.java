958bbc4cd8707f7476098e2e8f15451877bfd4b6
Hwangsj-qk <sujeong7191@naver.com>
2024-04-04 16:52:14 +0900

    클래스(Class) 공부

--- src/class0/class2/Student.java
+++ src/class0/class2/Student.java
@@ -22,5 +22,18 @@ public class Student {
     }
 
     // 메서드
-    // 초
+    // 초기화 메서드
+    void initStudnet(String nameParameter, int score, int age) {
+        // 필드와 매개변수의 이름이 같아서 데이터가 입력되지 않을 때
+
+        // 1. 필드와 매개변수의 이름을 다르게 설정
+        name = nameParameter;
+
+        // 2. this 키워드 활용
+        this.score = score;
+        this.age = age;
+
+        // 객체의 필드 값(age)에 매개변수(age)를 대입하겠다.
+        // this 는 객체 자신을 참조하는 참조 변수
+    }
 }
