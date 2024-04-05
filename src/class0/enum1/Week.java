bcd7fc4861f70e1b971d170208598ad54b42c8ec
Hwangsj-qk <sujeong7191@naver.com>
2024-04-05 16:03:40 +0900

    열거형(Enum) 공부

---
+++ src/class0/enum1/Week.java
@@ -0,0 +1,16 @@
+package class0.enum1;
+
+public enum Week {
+    /*
+    열거 타입: enumeration type
+    열거 상수의 집합을 저장하는 타입
+    열거 상수는 대문자로 작성한다.
+     */
+    SUNDAY,
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY
+}
